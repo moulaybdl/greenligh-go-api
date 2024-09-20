b2920388ffebb6c2cc30ddbf83d6171d8a039f15
moulaybdl <moulay.mohamed.bouabd.elli@ensia.edu.dz>
2024-09-21 00:30:47 +0200

    adding json responses

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/moulaybdl/greenligh-go-api/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +19,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	// create and return dummy data
+	movie := data.Movie {
+		ID: id,
+		CreatedAt: time.Now(),
+		Title: "Casablanca",
+		Runtime: 102,
+		Genres: []string{"drama", "romance", "war"},
+		Version: 1,
+	}
+
+	err = app.writeJSON(w, movie, http.StatusOK, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
