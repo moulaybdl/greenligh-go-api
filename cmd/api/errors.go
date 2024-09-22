41541f2d56c32c91bd776d614522386f3795234a
moulaybdl <moulay.mohamed.bouabd.elli@ensia.edu.dz>
2024-09-22 09:30:35 +0200

    handling JSON request

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -20,6 +20,15 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 	}
 }
 
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
 
--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -39,6 +42,63 @@ func (app *application) writeJSON(w  http.ResponseWriter,data envlope, status in
 	w.WriteHeader(status)
 	w.Write(js)
 
+	return nil
+
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+
+	
+	err := dec.Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError // problem with the syntax of json being decoded
+		var unmarshalTypeError *json.UnmarshalTypeError //JSON value type not compatible with the Go destination type
+		var invalidUnmarshalError *json.InvalidUnmarshalError // 	destination not valid (problem with the server side)
+
+
+
+		switch {
+			case errors.As(err, &syntaxError):
+				return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+			case errors.Is(err, io.ErrUnexpectedEOF):
+				return errors.New("body contain badly-formated JSON")
+
+			case errors.As(err, &unmarshalTypeError):
+				if unmarshalTypeError.Field != "" {
+					return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+				}
+				return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+			
+			case errors.Is(err, io.EOF):
+				return errors.New("body must not be empty")
+
+			case strings.HasPrefix(err.Error(), "json:unkown field "):
+				fieldName := strings.TrimPrefix(err.Error(), "json:unkown field")
+				return fmt.Errorf("body contains unkown key %s ", fieldName)
+
+			case err.Error() == "http: request body too large":
+				return fmt.Errorf("body must not be larget than %d bytes", maxBytes)
+			
+			case errors.As(err, &invalidUnmarshalError):
+				panic(err)
+
+			default:
+				return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
 	return nil
 
 }
